3964a8ca9eacef02eaa74a4d822cb6571aff5cf8
ApTeM1986 <103114269+ApTeM1986@users.noreply.github.com>
2022-06-27 12:30:42 +0200

    view

---
+++ Social Network Client/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Social Network Client
+//
+//  Created by Artem Stetsenko on 27.06.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginTextOulter: UITextField!
+    @IBOutlet weak var passTextOutlet: UITextField!
+ 
+    @IBOutlet weak var checkBoxOutlet: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+      
+        
+    }
+
+    @IBAction func pressedLoginButton(_ sender: Any) {
+    }
+    
+
+}
+
