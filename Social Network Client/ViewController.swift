a653f3e252985f4e05abba19a11d803a5cc6d541
ApTeM1986 <103114269+ApTeM1986@users.noreply.github.com>
2022-06-27 11:25:18 +0200

    Initial Commit

---
+++ Social Network Client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Social Network Client
+//
+//  Created by Artem Stetsenko on 27.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
