e3ea69ae21c8bf723fed074a3b512c531abc1bc4
ApTeM1986 <103114269+ApTeM1986@users.noreply.github.com>
2022-07-10 15:40:45 +0200

    Update readme.swift

--- Social Network Client/readme.swift
+++ Social Network Client/readme.swift
@@ -1,14 +1,1 @@
-//
-//  readme.swift
-//  Social Network Client
-//
-//  Created by Artem Stetsenko on 28.06.2022.
-//
-
-import Foundation
-
-// need to improve full screen view on friendVC
-// add scrollview to profile
-
-// id = 8214036
-
+// main branch is ApTeM1986-patch
